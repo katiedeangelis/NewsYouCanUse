6febe17b18ecb0fb783243c1eabe65c78b23d8c9
Katie DeAngelis <katie@Katies-MacBook-Pro.local>
2018-02-04 11:05:03 -0500

    Created basic file structure and got the server up and running

---
+++ server.js
@@ -0,0 +1,26 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+
+var port = process.env.PORT || 8080;
+
+var app = express();
+
+// Serve static content for the app from the "public" directory in the application directory.
+app.use(express.static("public"));
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// Set Handlebars.
+var exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+// Import routes and give the server access to them.
+var routes = require("./controller/site_router.js");
+
+app.use("/", routes);
+
+app.listen(port, function() {
+    console.log("listening on port", port);
+  });
